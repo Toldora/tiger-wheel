f82be0c21b9ea3a882a15111c6a750837c86f0a8
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-05-09 12:48:05 +0300

    validate email, added debug mode

--- src/main.js
+++ src/main.js
@@ -14,10 +14,10 @@ import { getFromLS } from '@/js/local-storage';
 
 useViewportSizes();
 
-const isAlreadyRegistered = getFromLS('isAlreadyRegistered');
-if (isAlreadyRegistered) {
-  const searchString = queryString.parse(window.location.search);
+const searchString = queryString.parse(window.location.search);
 
+const isAlreadyRegistered = getFromLS('isAlreadyRegistered');
+if (isAlreadyRegistered && !searchString.debug) {
   searchString['sign-in'] = true;
   const stringifiedSearch = queryString.stringify(searchString);
 
